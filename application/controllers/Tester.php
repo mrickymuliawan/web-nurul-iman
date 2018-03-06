8fff3c75b83671d5073932b2d2f9482145fbafc1
rakahikmah <rakahikmah46@gmail.com>
2018-03-06 17:57:25 +0700

    test hellow

---
+++ application/controllers/Tester.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tester extends CI_Controller {
+
+	public function testhello()
+	{
+		echo "hallo ini adalah test hello";
+	}
+
+}
+
+/* End of file Tester.php */
+/* Location: ./application/controllers/Tester.php */
